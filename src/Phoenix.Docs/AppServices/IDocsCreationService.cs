fcd70b499be91d1aff968aca088dd2e26bd32342
raisr <info@raisr.net>
2023-01-22 22:22:49 +0100

    Implemented DocsAdminController (#1)

--- src/Phoenix.Docs/AppServices/IDocsCreationService.cs
+++ src/Phoenix.Docs/AppServices/IDocsCreationService.cs
@@ -1,5 +1,6 @@
 ﻿using Phoenix.Docs.Results;
 using System.Threading.Tasks;
+using Phoenix.Docs.Domain;
 
 namespace Phoenix.Docs.AppServices;
 
@@ -8,11 +9,12 @@ public interface IDocsCreationService
     /// <summary>
     /// Creates the documentation for the project with the given shortname.
     /// </summary>
+    /// <param name="options"></param>
     /// <param name="project">
     /// The project.
     /// </param>
     /// <returns>
     /// Result containing the url for newly created documentation.
     /// </returns>
-    Task<Result<string>> CreateDocs(string project);
+    Task<Result<string>> CreateDocs(DocsOptions options, Project project);
 }
--- src/Phoenix.Docs/Controllers/DocsAdminController.cs
+++ src/Phoenix.Docs/Controllers/DocsAdminController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Phoenix.Docs.AppServices;
+using Phoenix.Docs.Domain;
+using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -12,22 +16,32 @@ namespace Phoenix.Docs.Controllers
         #region Fields
         
         private readonly IDocsCreationService docsCreationService;
+        private readonly DocsOptions options;
 
         #endregion
 
         #region Constructors
 
-        public DocsAdminController(IDocsCreationService docsCreationService)
+        public DocsAdminController(IDocsCreationService docsCreationService, IOptions<DocsOptions> options)
         {
             this.docsCreationService = docsCreationService;
+            this.options = options.Value;
         } 
         
         #endregion
 
-        [HttpPost]
-        public async Task<ActionResult<string>> CreateDocs(string project)
+        [HttpPost("shortName")]
+        public async Task<ActionResult<string>> CreateDocs(string shortName)
         {
-            var result = await this.docsCreationService.CreateDocs(project);
+            var project = this.options.Projects.SingleOrDefault(x =>
+                x.ShortName.Equals(shortName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            var result = await this.docsCreationService.CreateDocs(options, project);
             if (result.Succees)
             {
                 return Created(result.Value, null);
