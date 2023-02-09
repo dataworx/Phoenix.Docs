4f5ce02bd7bd315890d0880d8af95cae412b87f7
raisr <info@raisr.net>
2023-02-09 23:59:59 +0100

    Refactoring (#1)

--- src/Phoenix.Docs/Configuration/ServiceCollectionExtensions.cs
+++ src/Phoenix.Docs/Configuration/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Phoenix.Docs.Domain;
 using Phoenix.Docs.Publish;
 
 namespace Phoenix.Docs.Configuration
